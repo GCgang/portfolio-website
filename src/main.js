8ab632fd4c9bb75f5099b3f31ca706b89696e30e
GCgang <powm97@gmail.com>
2024-06-13 20:12:17 +0900

    Feat: JS-arrow up

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 window.addEventListener('scroll', () => {    
-    if (headerHeight < window.scrollY) {
+    if (window.scrollY > headerHeight) {
         header.classList.add('header--dark');
     } else {
         header.classList.remove('header--dark');
@@ -14,4 +14,14 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 window.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
+})
+
+// 페이지 아래로 스크롤 시 arrow up botton 스타일링 적용
+const arrowUp = document.querySelector('.arrow-up');
+window.addEventListener('scroll', () => {
+    if (window.scrollY > homeHeight / 2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
 })
