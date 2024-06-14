135c39d88e0f6541dacea75c87e12b759d9fe9ea
GCgang <powm97@gmail.com>
2024-06-14 17:35:31 +0900

    Refactor: JS-projects 필터링 함수로 분리

--- src/projects.js
+++ src/projects.js
@@ -10,20 +10,27 @@ categories.addEventListener('click', (event) => {
     if (!filter) {
         return;
     }
+    handleActiveSelection(event.target);
+    filterProjects(filter);
+})
 
+function handleActiveSelection(target) {
     const active = document.querySelector('.category--selected');
     active.classList.remove('category--selected');
     event.target.classList.add('category--selected');
+}
 
-    projectsContainer.classList.add('animation-out');
+function filterProjects(filter) {
     projects.forEach((project) => {
         if (filter === 'all' || filter === project.dataset.type) {
             project.style.display = 'block';
         } else {
             project.style.display = 'none';
         }
-    })
+    });
+    projectsContainer.classList.add('animation-out');
     setTimeout(() => {
         projectsContainer.classList.remove('animation-out');
     }, 250);
-})
+}
+
